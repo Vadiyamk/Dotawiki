8befe5c2475b7fa8410d5a44c712ea2a847bd09a
Vadiyamk <140454426+Vadiyamk@users.noreply.github.com>
2023-07-25 00:44:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(["index.html","style/style.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html", "cities.html", "tours.html", "contacts.html","assets/css/style.css", "assets/css/cities.css", "assets/css/tours.css", "assets/css/contacts.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
